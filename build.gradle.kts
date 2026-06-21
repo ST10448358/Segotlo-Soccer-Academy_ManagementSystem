0e44d13a027f8902dfb86814b980cd719d83aeb8
lesedimathunya <162095613+lesedimathunya@users.noreply.github.com>
2026-06-21 23:57:24 +0200

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,55 +1,7 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.google.gms.google.services)
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.google.gms.google.services) apply false
+
+
 }
-
-android {
-    namespace = "com.example.segotlo"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
-
-    defaultConfig {
-        applicationId = "com.example.segotlo"
-        minSdk = 24
-        targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-}
-
-dependencies {
-
-        implementation(libs.androidx.activity.ktx)
-        implementation(libs.androidx.appcompat)
-        implementation(libs.androidx.constraintlayout)
-        implementation(libs.androidx.core.ktx)
-
-        implementation(libs.firebase.firestore)
-        implementation(libs.firebase.auth)
-        implementation(libs.firebase.storage)
-
-        implementation(libs.material)
-
-        testImplementation(libs.junit)
-        androidTestImplementation(libs.androidx.espresso.core)
-        androidTestImplementation(libs.androidx.junit)
-    }
